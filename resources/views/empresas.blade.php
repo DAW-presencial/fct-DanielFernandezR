939866d1dea9f7a99051e6580ba2c1e8986f5656
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2021-02-10 17:09:17 +0100

    feat(empresas): Añadir oldValue al formulario

--- resources/views/empresas.blade.php
+++ resources/views/empresas.blade.php
@@ -16,7 +16,7 @@
         <div class="row">
             <div class="form-group col-4">
                 <label>Nombre</label>
-                <input type="text" class="form-control {{ $errors->has('nombre') ? 'error' : '' }}" name="nombre" id="nombre">
+                <input type="text" class="form-control {{ $errors->has('nombre') ? 'error' : '' }}" value="{!! old('nombre') !!}" name="nombre" id="nombre">
                 <!-- Error -->
                 @if ($errors->has('nombre'))
                 <div class="error">
@@ -26,7 +26,7 @@
             </div>
             <div class="form-group col-4">
                 <label>CIF</label>
-                <input type="text" class="form-control {{ $errors->has('cif') ? 'error' : '' }}" name="cif" id="cif">
+                <input type="text" class="form-control {{ $errors->has('cif') ? 'error' : '' }}" value="{!! old('cif') !!}" name="cif" id="cif">
                 @if ($errors->has('cif'))
                 <div class="error">
                     {{ $errors->first('cif') }}
@@ -35,7 +35,7 @@
             </div>
             <div class="form-group col-4">
                 <label>Email</label>
-                <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}" name="email" id="email">
+                <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}" value="{!! old('email') !!}" name="email" id="email">
                 @if ($errors->has('email'))
                 <div class="error">
                     {{ $errors->first('email') }}
@@ -46,7 +46,7 @@
         <div class="row">
             <div class="form-group col-4">
                 <label>Teléfono 1</label>
-                <input type="tel" class="form-control {{ $errors->has('telefono1') ? 'error' : '' }}" name="telefono1"
+                <input type="tel" class="form-control {{ $errors->has('telefono1') ? 'error' : '' }}" value="{!! old('telefono1') !!}" name="telefono1"
                     id="telefono1" maxlength="9">
                 @if ($errors->has('telefono1'))
                 <div class="error">
@@ -56,7 +56,7 @@
             </div>
             <div class="form-group col-4">
                 <label>Teléfono 2 (Opcional)</label>
-                <input type="text" class="form-control {{ $errors->has('telefono2') ? 'error' : '' }}" name="telefono2"
+                <input type="text" class="form-control {{ $errors->has('telefono2') ? 'error' : '' }}" value="{!! old('telefono2') !!}" name="telefono2"
                     id="telefono2">
                 @if ($errors->has('telefono2'))
                 <div class="error">
@@ -66,7 +66,7 @@
             </div>
             <div class="form-group col-4">
                 <label>Fax (Opcional)</label>
-                <input type="text" class="form-control {{ $errors->has('fax') ? 'error' : '' }}" name="fax" id="fax">
+                <input type="text" class="form-control {{ $errors->has('fax') ? 'error' : '' }}" value="{!! old('fax') !!}" name="fax" id="fax">
                 @if ($errors->has('fax'))
                 <div class="error">
                     {{ $errors->first('fax') }}
@@ -77,7 +77,7 @@
         <div class="row">
             <div class="form-group col-5">
                 <label>Dirección</label>
-                <input type="text" class="form-control {{ $errors->has('direccion') ? 'error' : '' }}" name="direccion" id="direccion">
+                <input type="text" class="form-control {{ $errors->has('direccion') ? 'error' : '' }}" value="{!! old('direccion') !!}" name="direccion" id="direccion">
                 @if ($errors->has('direccion'))
                 <div class="error">
                     {{ $errors->first('direccion') }}
@@ -86,7 +86,7 @@
             </div>
             <div class="form-group col-5">
                 <label>Población</label>
-                <input type="text" class="form-control {{ $errors->has('poblacion') ? 'error' : '' }}" name="poblacion"
+                <input type="text" class="form-control {{ $errors->has('poblacion') ? 'error' : '' }}" value="{!! old('poblacion') !!}" name="poblacion"
                     id="poblacion">
                 @if ($errors->has('poblacion'))
                 <div class="error">
@@ -96,7 +96,7 @@
             </div>
             <div class="form-group col-2">
                 <label>Código Postal</label>
-                <input type="text" class="form-control {{ $errors->has('cp') ? 'error' : '' }}" name="cp" id="cp">
+                <input type="text" class="form-control {{ $errors->has('cp') ? 'error' : '' }}" value="{!! old('cp') !!}" name="cp" id="cp">
                 @if ($errors->has('cp'))
                 <div class="error">
                     {{ $errors->first('cp') }}
@@ -107,7 +107,7 @@
         <div class="row">
             <div class="form-group col-8">
                 <label>Actividad Principal</label>
-                <input type="text" class="form-control {{ $errors->has('actividad_principal') ? 'error' : '' }}" name="actividad_principal"
+                <input type="text" class="form-control {{ $errors->has('actividad_principal') ? 'error' : '' }}" value="{!! old('actividad_principal') !!}" name="actividad_principal"
                     id="actividad_principal">
                 @if ($errors->has('actividad_principal'))
                 <div class="error">
@@ -118,9 +118,9 @@
             <div class="form-group col-4">
                 <label>Sector productivo</label>
                 <select class="form-control {{ $errors->has('sector_productivo') ? 'error' : '' }}" name="sector_productivo" id="sector_productivo">
-                    <option value="primario">Primario</option>
-                    <option value="secundario">Secundario</option>
-                    <option value="terciario">Terciario</option>
+                    <option value="primario" @if (old('sector_productivo') == 'primario') selected @endif>Primario</option>
+                    <option value="secundario" @if (old('sector_productivo') == 'secundario') selected @endif>Secundario</option>
+                    <option value="terciario" @if (old('sector_productivo') == 'terciario') selected @endif>Terciario</option>
                 </select>
                 @if ($errors->has('sector_productivo'))
                 <div class="error">
@@ -133,11 +133,11 @@
             <div class="form-group col-2">
                 <label>Titularidad</label>
                 <div>
-                    <input type="radio" class="{{ $errors->has('titularidad') ? 'error' : '' }}" name="titularidad" id="publica" value="publica" checked>
+                    <input type="radio" class="{{ $errors->has('titularidad') ? 'error' : '' }}" name="titularidad" id="publica" value="publica" @if (old('titularidad') == 'publica') checked @endif>
                     <label for="publica">Pública</label>
                 </div>
                 <div>
-                    <input type="radio" class="{{ $errors->has('titularidad') ? 'error' : '' }}" name="titularidad" id="privada" value="privada">
+                    <input type="radio" class="{{ $errors->has('titularidad') ? 'error' : '' }}" name="titularidad" id="privada" value="privada" @if (old('titularidad') == 'privada') checked @endif>
                     <label for="privada">Privada</label>
                 </div>
                 @if ($errors->has('titularidad'))
@@ -148,7 +148,7 @@
             </div>
             <div class="form-group col-5">
                 <label>Nombre del representante</label>
-                <input type="text" class="form-control {{ $errors->has('representante_nombre') ? 'error' : '' }}" name="representante_nombre"
+                <input type="text" class="form-control {{ $errors->has('representante_nombre') ? 'error' : '' }}" value="{!! old('representante_nombre') !!}" name="representante_nombre"
                     id="representante_nombre">
                 @if ($errors->has('representante_nombre'))
                 <div class="error">
@@ -158,7 +158,7 @@
             </div>
             <div class="form-group col-5">
                 <label>NIF del representante</label>
-                <input type="text" class="form-control {{ $errors->has('representante_nif') ? 'error' : '' }}" name="representante_nif"
+                <input type="text" class="form-control {{ $errors->has('representante_nif') ? 'error' : '' }}" value="{!! old('representante_nif') !!}" name="representante_nif"
                     id="representante_nif">
                 @if ($errors->has('representante_nif'))
                 <div class="error">
